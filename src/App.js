6af1850c7a618778d269b9b64ab416fce6070d77
nirajpatilcs <62448681+nirajpatilcs@users.noreply.github.com>
2022-01-10 19:54:25 +1300

    created state for new job
    added state object to store the new job data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from './components/Header'
 import NewjobForm from "./components/NewjobForm";
 
 const App = () => {
+
+  //state for adding new job 
+  const [newJob, setNewJob] = useState({
+    company: '', 
+    position: '',
+    notes: '',
+    status: ''
+  })
+  
+  //recieves new job data and stores it in state  
+  function handleNewJob(event) {
+    event.preventDefault()
+    const {company, position, notes, status } = event.target
+    setNewJob({ 
+        company: company.value, 
+        position: position.value, 
+        notes: notes.value, 
+        status: status.value
+      })
+    console.log(newJob)
+  }
+
   return (
     <div className="p-5">
       <Header/>
-      <NewjobForm/>
+      <NewjobForm handleNewJob={handleNewJob}/>
     </div>
   )
 };
--- src/components/NewjobForm.jsx
+++ src/components/NewjobForm.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
-export default function NewjobForm() {
+export default function NewjobForm(props) {
     return (
-        <div> 
-            new form here!
+        <div className='bg-white border-2 border-black  flex justify-center	 '> 
+            <form className='w-6/12' onSubmit={props.handleNewJob}> 
+                <label> <input type='text' placeholder='Company' name='company'/> </label>
+                <label> <input type='text' name='position' placeholder='Position'/> </label>
+
+                <label> <textarea type='text' name='notes' placeholder='Notes'/> </label>
+
+                <label> 
+                    <select name='status'>
+                        <option value='offer'>Offer</option> 
+                        <option defaultValue value='awaitingResponse'>Awaiting Response</option> 
+                        <option value='rejected'>Rejected</option> 
+                    </select> 
+                </label>
+
+                <button className='block'>Add Job</button> 
+            </form> 
         </div>
     )
 }
