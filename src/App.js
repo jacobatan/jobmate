607004a2fad34cb19571545dadf522373790961d
nirajpatilcs <62448681+nirajpatilcs@users.noreply.github.com>
2022-01-10 19:13:50 +1300

    added new job form component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import Header from './components/Header'
+import NewjobForm from "./components/NewjobForm";
 
 const App = () => {
-  return <div className="p-5"><Header/></div>;
+  return (
+    <div className="p-5">
+      <Header/>
+      <NewjobForm/>
+    </div>
+  )
 };
 
 export default App;
---
+++ src/components/NewjobForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NewjobForm() {
+    return (
+        <div> 
+            new form here!
+        </div>
+    )
+}
