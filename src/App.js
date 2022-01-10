6be07183dde227ca421da3ebcd006ecc6200de42
Jacob Tan <jacob.tan229@gmail.com>
2022-01-10 21:14:24 +1300

    Prettier :) + main card section

--- src/App.js
+++ src/App.js
@@ -1,36 +1,47 @@
-import React, {useState} from "react";
-import Header from './components/Header'
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Jobcard from "./components/Jobcard";
 import NewjobForm from "./components/NewjobForm";
 
 const App = () => {
-
-  //state for adding new job 
+  let foo = new Array(30).fill("bar"); // Testing...
+  console.log(foo);
+  //state for adding new job
   const [newJob, setNewJob] = useState({
-    company: '', 
-    position: '',
-    notes: '',
-    status: ''
-  })
-  
-  //recieves new job data and stores it in state  
+    company: "",
+    position: "",
+    notes: "",
+    status: "",
+  });
+
+  //recieves new job data and stores it in state
   function handleNewJob(event) {
-    event.preventDefault()
-    const {company, position, notes, status } = event.target
-    setNewJob({ 
-        company: company.value, 
-        position: position.value, 
-        notes: notes.value, 
-        status: status.value
-      })
-    console.log(newJob)
+    event.preventDefault();
+    const { company, position, notes, status } = event.target;
+    setNewJob({
+      company: company.value,
+      position: position.value,
+      notes: notes.value,
+      status: status.value,
+    });
+    console.log(newJob);
   }
 
   return (
-    <div className="p-5">
-      <Header/>
-      <NewjobForm handleNewJob={handleNewJob}/>
+    <div className="bg-gray-50 h-screen">
+      <Header />
+      <NewjobForm handleNewJob={handleNewJob} />
+
+      {/* main card */}
+
+      <section className="pt-12 grid grid-cols-1 md:max-w-sm md:grid-cols-2 lg:max-w-7xl  xl:grid-cols-3  mx-auto gap-x-6 gap-y-6">
+        {foo.map((foobar, i) => (
+          <Jobcard key={i} />
+        ))}
+        {/* ^^^^ temporary soln for testing purposes :) */}
+      </section>
     </div>
-  )
+  );
 };
 
 export default App;
