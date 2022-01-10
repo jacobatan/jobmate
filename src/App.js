8b765120fd254498424281181937132224f42600
Jacob Tan <jacob.tan229@gmail.com>
2022-01-11 11:13:08 +1300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,70 +11,83 @@ const App = () => {
     location: "",
     notes: "",
     status: "",
-    date: ""
+    date: "",
   };
   const [newJob, setNewJob] = useState(defaultNewJob);
-  const[showNewJobForm, setNewJobForm] = useState(false);
+  const [showNewJobForm, setNewJobForm] = useState(false);
 
   //stores all current jobs
   const [allJobs, setAllJobs] = useState(
-    JSON.parse(localStorage.getItem('allJobs')) || []
-  )
+    JSON.parse(localStorage.getItem("allJobs")) || []
+  );
 
   //recieves new job data and stores it in state, newJob
   function handleNewJob(event) {
-    setNewJob(prevJobData => {
+    setNewJob((prevJobData) => {
       return {
-          ...prevJobData,
-          [event.target.name]: event.target.value
-      } 
+        ...prevJobData,
+        [event.target.name]: event.target.value,
+      };
     });
   }
 
   //everytime new job is added, update local storage
-  React.useEffect(()=> {
-    localStorage.setItem('allJobs', JSON.stringify(allJobs))
-  }, [allJobs])
+  React.useEffect(() => {
+    localStorage.setItem("allJobs", JSON.stringify(allJobs));
+  }, [allJobs]);
 
   //uses the upadated new job state to create a new Jobcard Component
   function renderNewJob(event) {
-    event.preventDefault()
-    const latestJob = newJob
+    event.preventDefault();
+    const latestJob = newJob;
     //  update all jobs
-    setAllJobs(allPrevJobs => {return [...allPrevJobs, latestJob]})
+    setAllJobs((allPrevJobs) => {
+      return [...allPrevJobs, latestJob];
+    });
     // !!!!!!!!!!!!!!!FOR SOME REASON DATE DOESNT get displayed !!!!!!!//
-    setNewJob(defaultNewJob) //clear the form and default the state
-    setNewJobForm(old=> !old)
+    setNewJob(defaultNewJob); //clear the form and default the state
+    setNewJobForm((old) => !old);
   }
 
   // need to implement the delete job button
   function handleJobDelete(id) {
-    console.log(`delete ${id}`)
+    console.log(`delete ${id}`);
   }
 
   //maps over all jobs and renders the jsx
-  const renderAllJobs = allJobs.map((job,i)=> {
+  const renderAllJobs = allJobs.map((job, i) => {
     return (
-      <Jobcard key={i} newJob={job} handleJobDelete={()=>handleJobDelete(i)}/>
-    )
-  })
-
+      <Jobcard
+        key={i}
+        newJob={job}
+        handleJobDelete={() => handleJobDelete(i)}
+      />
+    );
+  });
 
   function toggleModal() {
-    setNewJobForm(old=> !old)
+    setNewJobForm((old) => !old);
   }
 
   return (
     <div className="bg-gray-50 h-screen">
       <Header />
-      <NewjobForm showNewJobForm={showNewJobForm} handleNewJob={handleNewJob} newJob={newJob} renderNewJob={renderNewJob} toggleModal={toggleModal}/>
+      <NewjobForm
+        showNewJobForm={showNewJobForm}
+        handleNewJob={handleNewJob}
+        newJob={newJob}
+        renderNewJob={renderNewJob}
+        toggleModal={toggleModal}
+      />
 
       {/* main card */}
 
       <section className="pt-12 grid grid-cols-1 md:max-w-sm md:grid-cols-2 lg:max-w-7xl  xl:grid-cols-3  mx-auto gap-x-6 gap-y-6">
-
         {renderAllJobs}
-        <button className="border-2 cursor-pointer" onClick={toggleModal} > Add New Job </button>
+        <button className="border-2 cursor-pointer" onClick={toggleModal}>
+          {" "}
+          Add New Job{" "}
+        </button>
       </section>
     </div>
   );
