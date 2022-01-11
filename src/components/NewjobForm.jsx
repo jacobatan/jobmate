510852b1f9467aba2e4f3e05ef31cf3f2390eea1
Jacob Tan <jacob.tan229@gmail.com>
2022-01-11 18:51:31 +1300

    When clicking on the background, the modal closes

--- src/components/NewjobForm.jsx
+++ src/components/NewjobForm.jsx
@@ -7,6 +7,7 @@ export default function NewjobForm(props) {
         props.showNewJobForm ? "inline-flex" : "hidden"
       } justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none 
       focus:outline-none cursor-pointer p-10 h-screen bg-slate-600/50 `}
+      onClick={props.toggleModal}
     >
       {/* contents */}
       <div className="border-0 rounded-2xl shadow-2xl relative flex flex-col w-2/5 bg-white outline-none cursor-default p-8">
