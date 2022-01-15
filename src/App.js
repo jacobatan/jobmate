cbe7c1b48b366caafa9a87aef48d49d245ebb9f1
Jacob Tan <jacob.tan229@gmail.com>
2022-01-16 12:18:30 +1300

    firebase

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import Jobcard from "./components/Jobcard";
 import NewjobForm from "./components/NewjobForm";
 import Summary from "./components/Summary";
 import Login from "./components/Login";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlusCircle } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
+import { db } from "./firebase";
+import { getDocs, collection, addDoc } from "firebase/firestore";
 
 const App = () => {
   //state for adding new job
@@ -14,43 +16,70 @@ const App = () => {
     position: "",
     location: "",
     notes: "",
-    status: 'awaitingResponse',
+    status: "awaitingResponse",
     date: "",
   };
   const [newJob, setNewJob] = useState(defaultNewJob);
   const [showNewJobForm, setNewJobForm] = useState(false);
   const [openModal, setOpenModal] = useState(false);
-  const [loginSuccess, setLoginSuccess] = useState(false);
-  const [edit, setEdit] = useState(
-      {
-        edit: false, 
-        id: null
-      });
-  const [summary, setSummary] = useState(
-    {
-      offers: 0, 
-      awaitingResponse: 0
-    }
-  )
-
+  const [loginSuccess, setLoginSuccess] = useState(true);
+  const [edit, setEdit] = useState({
+    edit: false,
+    id: null,
+  });
+  const [summary, setSummary] = useState({
+    offers: 0,
+    awaitingResponse: 0,
+  });
   //stores all current jobs
   const [allJobs, setAllJobs] = useState(
     JSON.parse(localStorage.getItem("allJobs")) || []
   );
+  // --------------- FIREBASE STUFF THAT ACTUALLY FUCKING WORKS ------------------ //
 
-  //everytime new job is added, update local storage
+  const colRef = collection(db, "gamers");
   useEffect(() => {
-    localStorage.setItem("allJobs", JSON.stringify(allJobs));
-    const offers = allJobs.filter(job => job.status === 'offer').length;
-    const awaitingResponse = allJobs.filter(job => job.status === 'awaitingResponse').length;
+    async function getGamers() {
+      // grab that snapshot
+      const snapshot = await getDocs(colRef);
+      // this maps the datas to the list var
+      let gamerSnapshot = snapshot.docs.map((doc) => {
+        return { ...doc.data(), id: doc.id };
+      });
+      console.log(gamerSnapshot);
+    }
+    getGamers();
+  }, [newJob]);
 
-    setSummary({offers: offers, awaitingResponse: awaitingResponse})
-  }, [allJobs]);
+  //uses the upadated new job state to create a new Jobcard Component
+  function renderNewJob(event) {
+    event.preventDefault();
+    console.log("renderNewJob has been called!!");
+    const latestJob = newJob; //  update all jobs
+    addDoc(colRef, {
+      company: latestJob.company,
+      position: latestJob.position,
+      location: latestJob.location,
+      notes: latestJob.notes,
+      status: latestJob.status,
+      date: latestJob.date,
+    });
+    if (edit.edit) {
+      handleJobEdit(edit.id);
+    } else {
+      setAllJobs((allPrevJobs) => [...allPrevJobs, latestJob]);
+      setNewJob(defaultNewJob); //clear the form and default the state
+      setNewJobForm((old) => !old);
+    }
+  }
 
-
+  // --------------- FIREBASE STUFF THAT ACTUALLY FUCKING WORKS ------------------ //
 
   //recieves new job data and stores it in state, newJob
   function handleNewJob(event) {
+    console.log("handleNewJob has been called");
+    // This lets us add shit to the database the id is always randomised.
+
     setNewJob((prevJobData) => {
       return {
         ...prevJobData,
@@ -59,24 +88,19 @@ const App = () => {
     });
   }
 
+  //everytime new job is added, update local storage
+  useEffect(() => {
+    localStorage.setItem("allJobs", JSON.stringify(allJobs));
+    const offers = allJobs.filter((job) => job.status === "offer").length;
+    const awaitingResponse = allJobs.filter(
+      (job) => job.status === "awaitingResponse"
+    ).length;
 
-  //uses the upadated new job state to create a new Jobcard Component
-  function renderNewJob(event) {
-    event.preventDefault();
-    const latestJob = newJob;
-    //  update all jobs
-    if(edit.edit) {
-      handleJobEdit(edit.id)
-    } else {
-      setAllJobs((allPrevJobs) => [...allPrevJobs, latestJob]);
-      setNewJob(defaultNewJob); //clear the form and default the state
-      setNewJobForm((old) => !old);
-    }
-
-  }
+    setSummary({ offers: offers, awaitingResponse: awaitingResponse });
+  }, [allJobs]);
 
   function handleJobDelete(id) {
-    const updatedJobs = [...allJobs]
+    const updatedJobs = [...allJobs];
     const newJobs = updatedJobs.filter((_, i) => i != id);
     setAllJobs(newJobs);
   }
@@ -87,25 +111,27 @@ const App = () => {
   }
 
   function handleJobEdit(id) {
-    setAllJobs(prevJobs => prevJobs.map((job, i) => {
-      if(i===id) {
-        return {
-          ...newJob
+    setAllJobs((prevJobs) =>
+      prevJobs.map((job, i) => {
+        if (i === id) {
+          return {
+            ...newJob,
+          };
+        } else {
+          return job;
         }
-      } else{
-        return job
-      }
-    }))
-    setEdit({edit: false, id: null})
+      })
+    );
+    setEdit({ edit: false, id: null });
     setNewJob(defaultNewJob); //clear the form and default the state
     setNewJobForm((old) => !old);
-    setOpenModal(old=>!old)
+    setOpenModal((old) => !old);
   }
 
   function editJobCard(id) {
-    setEdit({edit: true, id: id}); //state which tracks if its a edit or new job
-    setNewJob(allJobs[id]) //sets values of form to old values 
-    setOpenModal(old=>!old)
+    setEdit({ edit: true, id: id }); //state which tracks if its a edit or new job
+    setNewJob(allJobs[id]); //sets values of form to old values
+    setOpenModal((old) => !old);
   }
 
   //maps over all jobs and renders the jsx
@@ -115,7 +141,7 @@ const App = () => {
         key={i}
         newJob={job}
         handleJobDelete={() => handleJobDelete(i)}
-        editJobCard = {() => editJobCard(i)}
+        editJobCard={() => editJobCard(i)}
       />
     );
   });
@@ -128,7 +154,7 @@ const App = () => {
       {loginSuccess && (
         <div>
           <Header />
-          <Summary summary={summary}/>
+          <Summary summary={summary} />
           {openModal && (
             <NewjobForm
               showNewJobForm={showNewJobForm}
@@ -145,7 +171,10 @@ const App = () => {
               className="border-2 hover:blur-sm hover:shadow-lg focus:blur-sm focus:shadow-lg  border-dashed cursor-pointer text-xl opacity-60"
               onClick={() => setOpenModal(true)}
             >
-              <FontAwesomeIcon icon={faPlusCircle} className="my-2 fa-2x block mx-auto"/>
+              <FontAwesomeIcon
+                icon={faPlusCircle}
+                className="my-2 fa-2x block mx-auto"
+              />
             </button>
             {renderAllJobs}
           </section>
