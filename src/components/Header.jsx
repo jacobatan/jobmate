60d6390e1b00b2feb108e1fc1f3e1e4b8b2bda3f
Jacob Tan <jacob.tan229@gmail.com>
2022-01-22 14:27:42 +1300

    skeleton

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,11 +11,14 @@ const Header = ({ photoURL }) => {
     <div className="w-full border-b py-4 mb-4 flex justify-between">
       <h1 className="text-xl font-medium ml-8">JobMate</h1>
       <div onClick={toggleProfileOptions} className="relative">
-        <img
-          className="w-10 h-10 object-cover rounded-full mr-20 cursor-pointer"
-          src={photoURL}
-          alt="profile photo"
-        />
+        {photoURL ? (
+          <img
+            className="w-10 h-10 object-cover rounded-full mr-20 cursor-pointer"
+            src={photoURL}
+          />
+        ) : (
+          <div className="w-10 h-10 rounded-full mr-20 bg-gray-300 animate-pulse" />
+        )}
         {displayProfileOptions && (
           <div className="absolute right-2 flex flex-col shadow-sm bg-white z-10 min-w-max">
             <span className="p-4 w-max"> Edit Profile</span>
